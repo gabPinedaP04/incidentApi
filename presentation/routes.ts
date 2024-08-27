9b569c4a4a3ed7e2c83c6c75decdb2f678080ab8
gabPinedaP04 <gab.pinedap04@gmail.com>
2024-08-26 19:43:32 -0600

    modify gitignore

---
+++ presentation/routes.ts
@@ -0,0 +1,9 @@
+import {Router} from 'express';
+
+export class AppRoutes {
+    static get routes() : Router{
+        const router = Router();
+        router.use("/api/incidents",)
+        return router
+    }
+}
